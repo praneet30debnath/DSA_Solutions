72150eeca66abbe680daca47ac3616ea5f8dba5e
Praneet Debnath <105339841+praneet30debnath@users.noreply.github.com>
2025-04-04 15:44:45 +0530

    Update 02_FrogJump.java

--- DP/Solutions/02_FrogJump.java
+++ DP/Solutions/02_FrogJump.java
@@ -2,22 +2,45 @@
 //https://takeuforward.org/data-structure/dynamic-programming-frog-jump-dp-3/
 
 class Solution {
-    static int solve(int ind,int[] height,int[] dp){
+    private static final String MEMOIZATION = "MEMOIZATION";
+    private static final String TABULAR = "TABULAR";
+    
+    static int solveMemoization(int ind,int[] height,int[] dp){
         if(ind==0) return 0;
         if(dp[ind]!=-1) return dp[ind];
         int jumpTwo = Integer.MAX_VALUE;
-        int jumpOne= solve(ind-1, height,dp)+ Math.abs(height[ind]-height[ind-1]);
+        int jumpOne= solveMemoization(ind-1, height,dp)+ Math.abs(height[ind]-height[ind-1]);
         if(ind>1)
-            jumpTwo = solve(ind-2, height,dp)+ Math.abs(height[ind]-height[ind-2]);
+            jumpTwo = solveMemoization(ind-2, height,dp)+ Math.abs(height[ind]-height[ind-2]);
         
         return dp[ind]=Math.min(jumpOne, jumpTwo);
     }
     
+    static int solveTabular(int[] dp, int[] height) {
+        dp[0] = 0;
+        for(int ind=1;ind<dp.length;ind++) {
+            int firstStep = dp[ind-1] + Math.abs(height[ind]-height[ind-1]);
+            int secondStep = Integer.MAX_VALUE;
+            if(ind-2>=0)
+                secondStep = dp[ind-2] + Math.abs(height[ind]-height[ind-2]);
+                
+            dp[ind] = Math.min(firstStep,secondStep);
+        }
+        return dp[dp.length-1];
+    }
+    
     int minCost(int[] height) {
+        String approach = MEMOIZATION;
+        
         int[] dp = new int[height.length];
         for(int i=0;i<dp.length;i++) {
             dp[i] = -1;
         }
-        return solve(height.length-1,height,dp);
+        
+        if(approach.equals(MEMOIZATION))
+            return solveMemoization(height.length-1,height,dp);
+        if(approach.equals(TABULAR))
+            return solveTabular(dp,height);
+        return 0;
     }
 }
